e8087ac2750b35fb1a6bcbbf4c157000f333e2fb
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-06-03 10:23:22 -0300

    feat: testing

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductImage, ProductCard } from '../../src/components';
+import { product2 } from '../data/products';
+
+describe('ProductImage', () => {
+  test('Debe de mostrar el componente correctamente con la imagen personalizada', () => {
+    const wrapper = renderer.create(<ProductImage img="https://hola.jpg" />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  test('Debe de mostrar el componente correctamente con la imagen del producto', () => {
+    const wrapper = renderer.create(
+      <ProductCard product={product2}>{() => <ProductImage />}</ProductCard>
+    );
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+});
